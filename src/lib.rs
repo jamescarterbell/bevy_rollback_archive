57e7e2ceb39a69d8af80b39993ae780b4dbf4f26
James <jamescarterbell@gmail.com>
2020-12-30 18:01:43 -0500

    Basic stage stuff in place

--- src/lib.rs
+++ src/lib.rs
@@ -32,8 +32,6 @@ impl Plugin for RollbackPlugin{
 
 struct RollbackStage{
     schedule: Schedule,
-    rollback_run_criteria: Option<Box<dyn System<In = (), Out = ShouldRun>>>,
-    rollback_run_criteria_initialized: bool,
     run_criteria: Option<Box<dyn System<In = (), Out = ShouldRun>>>,
     run_criteria_initialized: bool,
     
@@ -43,8 +41,6 @@ impl RollbackStage{
     fn with_schedule(schedule: Schedule) -> Self{
         Self{
             schedule,
-            rollback_run_criteria: None,
-            rollback_run_criteria_initialized: false,
             run_criteria: None,
             run_criteria_initialized: false,
         }
@@ -53,26 +49,58 @@ impl RollbackStage{
     fn new() -> Self{
         Self{
             schedule: Schedule::default(),
-            rollback_run_criteria: None,
-            rollback_run_criteria_initialized: false,
             run_criteria: None,
             run_criteria_initialized: false,
         }
     }
 
-    fn run_once(&mut self, world: &mut World, resource: &mut Resources){
+    fn run_once(&mut self, world: &mut World, resources: &mut Resources){
+        self.schedule.run_once(world, resources);
+    }
 
+    fn run_rollback(&mut self, world: &mut World, resources: &mut Resources){
+        loop{
+            
+            let current_state = resources
+                .get::<RollbackBuffer>()
+                .expect("Couldn't find RollbackBuffer!")
+                .rollback_state;
+
+            match current_state{
+                RollbackState::Rollback(state) => {
+                    // Perform initial rollback
+                    
+                    // Setup for catchup
+                    resources
+                        .get_mut::<RollbackBuffer>()
+                        .expect("Couldn't find RollbackBuffer!")
+                        .rollback_state = RollbackState::Rolledback(state);
+                },
+                RollbackState::Rolledback(state) => {
+                    // Run schedule for state_n
+                    self.run_once(world, resources);
+            
+                    // Store state_n+1
+            
+                    // Increment counters
+                    let mut rollback_buffer = resources
+                        .get_mut::<RollbackBuffer>()
+                        .expect("Couldn't find RollbackBuffer!");
+                    let new_state = state + 1;
+                    rollback_buffer.rollback_state = RollbackState::Rolledback(new_state);
+                
+                    if new_state == rollback_buffer.newest_frame{
+                        // We're all caugt up!
+                        break;
+                    }
+                }
+            };
+        };
     }
 }
 
 impl Stage for RollbackStage{
     fn initialize(&mut self, world: &mut World, resources: &mut Resources){
-        if let Some(ref mut rollback_run_criteria) = self.rollback_run_criteria{
-            if !self.rollback_run_criteria_initialized{
-                rollback_run_criteria.initialize(world, resources);
-                self.rollback_run_criteria_initialized = true;
-            }
-        }
         if let Some(ref mut run_criteria) = self.run_criteria{
             if !self.run_criteria_initialized{
                 run_criteria.initialize(world, resources);
@@ -84,42 +112,56 @@ impl Stage for RollbackStage{
 
     fn run(&mut self, world: &mut World, resources: &mut Resources){
         loop{
-            // Check rollback condition
-            let should_run = if let Some(ref mut rollback_run_criteria) = self.rollback_run_criteria{
-                let should_run = rollback_run_criteria.run((), world, resources);
-                rollback_run_criteria.run_thread_local(world, resources);
+            // Check timestep condition
+            let should_run = if let Some(ref mut run_criteria) = self.run_criteria{
+                let should_run = run_criteria.run((), world, resources);
+                run_criteria.run_thread_local(world, resources);
                 should_run.unwrap_or(ShouldRun::No)
             } else {
                 ShouldRun::No
             };
-            // Check timestep condition after we're done rolling back
+            // Check rollback condition during the fixed time step
             match should_run{
-                ShouldRun::No=>{
-                    let should_run = if let Some(ref mut run_criteria) = self.run_criteria{
-                        let should_run = run_criteria.run((), world, resources);
-                        run_criteria.run_thread_local(world, resources);
-                        should_run.unwrap_or(ShouldRun::No)
-                    } else {
-                        ShouldRun::Yes
-                    };
-
-                    match should_run{
-                        ShouldRun::No => return,
-                        ShouldRun::Yes => {
-                            self.run_once(world, resources);
-                            return;
-                        }
-                        ShouldRun::YesAndLoop => {
-                            self.run_once(world, resources);
-                        }
-                    }
-                }
+                ShouldRun::No => return,
                 ShouldRun::Yes=>{
-                    self.run_once(world, resources);
+                    resources.get_mut::<RollbackBuffer>().expect("Couldn't find RollbackBuffer!").newest_frame += 1;
+                    self.run_rollback(world, resources);
                     return;
                 }
-                ShouldRun::YesAndLoop => self.run_once(world, resources),
+                ShouldRun::YesAndLoop => {
+                    resources.get_mut::<RollbackBuffer>().expect("Couldn't find RollbackBuffer!").newest_frame += 1;
+                    self.run_rollback(world, resources);
+                    continue;
+                },
             }   
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum RollbackState{
+    // Rollback to this state
+    Rollback(usize),
+    // Currently rolledback to this state, this could be the newest frame which is okay
+    Rolledback(usize),
+}
+
+struct RollbackBuffer{
+    newest_frame: usize,
+    rollback_state: RollbackState,
+
+    scenes: Vec<Scene>,
+    resources: Vec<Resources>,   
+}
+
+impl RollbackBuffer{
+    pub fn new(buffer_size: usize) -> Self{
+        RollbackBuffer{
+            newest_frame: 0,
+            rollback_state: RollbackState::Rolledback(0),
+
+            scenes: Vec::with_capacity(buffer_size),
+            resources: Vec::with_capacity(buffer_size),
         }
     }
 }
